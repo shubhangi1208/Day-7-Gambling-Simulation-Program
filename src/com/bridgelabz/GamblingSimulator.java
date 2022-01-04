dbba13772801f9ba9a0001a12445ffef9ebe19b3
shubhangi1208 <ashubhangi1996@gmail.com>
2022-01-04 15:01:24 +0530

    ADDED: Stake Per Day And Bet Per Game Constants

---
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+import java.util.*;
+
+public class GamblingSimulator {
+	Scanner sc = new Scanner(System.in);
+	private static final int stakeCost = 100;
+    private static final int stakeBetCost = 1;
+    
+	public static void main(String[] args) {
+		
+	       System.out.println("Welcome to the Gambling Simulator!");
+		
+	}
+
+}
