ae539c29363aa5ac6163c4cb3d59b9e2f09a9147
shubhangi1208 <ashubhangi1996@gmail.com>
2022-01-04 15:14:27 +0530

    MODIFIED:Win_loss_functionality

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -9,7 +9,21 @@ public class GamblingSimulator {
 	public static void main(String[] args) {
 		
 	       System.out.println("Welcome to the Gambling Simulator!");
-		
+//	        initializing random to generate values for win or loss decision
+	        Random random = new Random();
+	        int betResult = random.nextInt(2);
+//	        a function to check if the gambler wins or looses
+	        winOrLoss(betResult);
 	}
-
+	
+	 static void winOrLoss(int betResult){
+//       win
+       if(betResult == 1){
+           System.out.println("Gambler Wins!");
+       }
+//       loss
+       else{
+           System.out.println("Gambler Losses!");
+       }
+  }
 }
